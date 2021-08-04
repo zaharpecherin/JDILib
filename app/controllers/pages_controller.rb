4e4fd3717d3dbb006c150fae54fddd58d55480fb
Zahar Pecherin <zaharpecherin88@gmail.com>
2021-08-04 11:53:20 +0300

    Added http_basic_authenticate_with

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  http_basic_authenticate_with name: ENV["LOGIN_NAME"],
+                               password: ENV["LOGIN_PASSWORD"],
+                               except: :index unless Rails.env.development?
+
   def index; end
 
   def css; end
